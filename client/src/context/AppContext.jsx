d515f1f6df8d14cc1537ba0d2fc3ad020578e009
adi7406 <tayadeaditya451@gmail.com>
2026-08-19 01:36:46 +0530

    Added hero section and joblisting components

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,9 +1,19 @@
-import { createContext } from "react";
+import { createContext,useState } from "react";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
-    const value = {};
+    const [searchFilter , setSearchFilter] = useState({
+        title:'',
+        location:''
+    });
+
+    const [isSearched, setIsSearched] = useState(false);
+
+    const value = {
+        setSearchFilter,searchFilter,
+        setIsSearched ,isSearched,
+    };
 
     return (
         <AppContext.Provider value={value}>
