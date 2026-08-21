7beead35fe698f9bf386a317386955e3f0480d93
adi7406 <tayadeaditya451@gmail.com>
2026-08-21 19:08:17 +0530

    completed ApplyJob page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { assets } from "../assets/assets.js";
 import { useClerk, UserButton, useUser } from "@clerk/react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const { openSignIn } = useClerk();
   const { user } = useUser();
+  const navigate = useNavigate();
   return (
     <div className="shadow py-4">
       <div className="container px-4 2xl:px-20 mx-auto flex justify-between items-center">
-        <img src={assets.logo} alt="Job Portal logo" />
+        <img onClick={()=>navigate('/')} className="cursor-pointer" src={assets.logo} alt="Job Portal logo" />
         {user ? (
           <div className="flex items-center gap-3">
             <Link to={"/applications"}>Applied Jobs</Link>
             <p>|</p>
-            <p className="max-sm:hidden">Hi, {user.firstName+" "+user.lastName}</p>
+            <p className="max-sm:hidden">Hi, {user.firstName || ""} {user.lastName || ""}</p>
             <UserButton/>
           </div>
         ) : (
